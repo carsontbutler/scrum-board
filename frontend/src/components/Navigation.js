c4777c11d03d5cfbdf1ae1052eba4ece6ce46a09
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2022-06-14 12:32:02 -0500

    context branch test

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -12,7 +12,7 @@ const Navigation = (props) => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Scrum</Navbar.Brand>
+        <Navbar.Brand href="#home">Scrum (datacontext)</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav>
