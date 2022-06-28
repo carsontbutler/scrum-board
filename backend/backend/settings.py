ab91bfc6ba1bbb13e9b6f798365e85433f844827
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2022-06-28 17:49:20 -0500

    test ignore 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
-#test ignore
+#test ignore2
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
