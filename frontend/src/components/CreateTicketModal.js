f0f0a67c23bb0f77675c6a4cee37af3680fe441f
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2022-07-11 20:45:17 -0500

    untracked files

---
+++ frontend/src/components/CreateTicketModal.js
@@ -0,0 +1,36 @@
+import react from "react";
+import { Modal, Row, Col, Container, Button } from "react-bootstrap";
+import CreateTicketForm from "./Forms/CreateTicketForm";
+
+const CreateTicketModal = (props) => {
+  console.log(props);
+  return (
+    <Modal size={"xl"} centered show={props.showCreateTicketModal}>
+      <Modal.Header closeButton onHide={props.closeCreateTicketModalHandler}>
+        <Modal.Title>Creating new ticket</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Container>
+          <CreateTicketForm
+            activeOrganization={props.activeOrganization}
+            activeBoardData={props.activeBoardData}
+            closeCreateTicketModalHandler={props.closeCreateTicketModalHandler}
+            getBoardData={props.getBoardData}
+          />
+        </Container>
+      </Modal.Body>
+      <Modal.Footer className="justify-content-center">
+        <Button
+          onClick={props.closeCreateTicketModalHandler}
+          variant="secondary"
+        >
+          Cancel
+        </Button>
+
+        <Button type="submit" form="createTicketForm" variant="primary">Save</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default CreateTicketModal;
