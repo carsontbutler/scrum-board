81449af1718a93e33a0f6939b5bd06a817d8cf38
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2022-07-13 18:20:13 -0500

    styled select organization card

--- frontend/src/components/pages/SelectOrganization.js
+++ frontend/src/components/pages/SelectOrganization.js
@@ -4,6 +4,7 @@ import AuthContext from "../store/auth-context";
 import DataContext from "../store/data-context";
 import { Button, Row, Container, Col } from "react-bootstrap";
 import { axiosInstance, url } from "../store/api";
+import "./SelectScreen.css";
 
 const SelectOrganization = (props) => {
   const authCtx = useContext(AuthContext);
@@ -13,12 +14,15 @@ const SelectOrganization = (props) => {
       <Row className="justify-content-center mt-3">
         <Col xl={8} lg={8} md={10} sm={12} xs={12}>
           <div className="selection-card p-2">
-            <h1 className="text-center">Your organizations</h1>
+            <div className="card-header">
+              <h1 className="text-center">Your organizations</h1>
+            </div>
+
             <Row>
               <Col></Col>
               <Col xl={8} lg={8} md={10} sm={12} xs={12}>
                 {props.data.organizations.map((org) => (
-                  <Row>
+                  <Row className="select-buttons">
                     <Button
                       onClick={props.api.setOrganization}
                       key={org.id}
@@ -29,7 +33,7 @@ const SelectOrganization = (props) => {
                     </Button>
                   </Row>
                 ))}
-                <Row>
+                <Row className="new-button">
                   <Button size="lg">New</Button>
                 </Row>
               </Col>
