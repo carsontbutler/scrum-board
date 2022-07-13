9dcee758f5fe15270759625e419f2534178d41f9
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2022-07-13 13:18:53 -0500

    new auth page setup

--- frontend/src/components/pages/auth/AuthPage.js
+++ frontend/src/components/pages/auth/AuthPage.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginForm from "../../Forms/LoginForm";
 
 const AuthPage = () => {
+  const [errorMessage, setErrorMessage] = useState("");
   const [isLoggingIn, setIsLoggingIn] = useState(true);
-  const showLoginForm = () => { setIsLoggingIn(true) };
-  const showRegisterForm = () => { setIsLoggingIn(false) };
+  const showLoginForm = () => {
+    setIsLoggingIn(true);
+  };
+  const showRegisterForm = () => {
+    setIsLoggingIn(false);
+  };
+
   return (
     <div
       className="d-flex align-items-center justify-content-center"
@@ -13,8 +19,14 @@ const AuthPage = () => {
         height: "100vh",
       }}
     >
-      {isLoggingIn ? (<LoginForm />) : (<h6>RegisterForm</h6>)}
-
+      {isLoggingIn ? (
+        <LoginForm
+          errorMessage={errorMessage}
+          setErrorMessage={setErrorMessage}
+        />
+      ) : (
+        <h6>RegisterForm</h6>
+      )}
     </div>
   );
 };
