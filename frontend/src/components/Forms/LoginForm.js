83a4bee90e4730cb96c8ce59df2cc8015a8787ba
Carson Work <cbutler@rrs360.com>
2022-07-13 08:27:19 -0500

    added auth-container class

--- frontend/src/components/Forms/LoginForm.js
+++ frontend/src/components/Forms/LoginForm.js
@@ -41,7 +41,7 @@ const LoginForm = () => {
   };
 
   return (
-    <div style={{background: "linear-gradient(#FFF, #C0C0C0 80%)"}} className="p-5 rounded">
+    <div className="auth-container p-5 rounded">
       <h2 className="px-4">Scrum board</h2>
       <form onSubmit={handleSubmit} className="mt-4 text-center">
         <input
