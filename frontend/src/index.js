02b4a51b4abb3c2a5d92c2ab48870a123bd8b665
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2022-07-27 21:20:58 -0500

    cleanup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./components/store/auth-context";
-import { DataContextProvider } from "./components/store/data-context";
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextProvider>
-      <DataContextProvider>
         <App />
-      </DataContextProvider>
     </AuthContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
