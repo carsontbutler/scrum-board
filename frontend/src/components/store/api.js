23253cb99b605d2f93ac3017ac42ab06fb565377
carsontbutler <72775985+carsontbutler@users.noreply.github.com>
2022-09-07 19:21:48 -0500

    changed default url for prod

--- frontend/src/components/store/api.js
+++ frontend/src/components/store/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// export const url = "https://scrum-api.carsonbutler.dev/api";
+export const url = "https://scrum-api.carsonbutler.dev/api";
 
-export const url = "http://localhost:8000/api";
+// export const url = "http://localhost:8000/api";
 
 export const axiosInstance = axios.create({
   baseURL: url,
